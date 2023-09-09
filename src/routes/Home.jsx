0e8c0f1e9ad974a0414b07e5886902122918074f
Cauã Alencar Rojas Romero <cauarojas@hotmail.com>
2023-09-09 18:44:41 -0300

    added content to home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,15 @@
+import { Link } from 'react-router-dom'
 import '../styles/Home.css'
 export default function Home() {
     return (
         <>
             <main className="home">
-                <h1>Home</h1>
+                <article>
+                    <h4>Porto Seguro</h4>
+                    <h1>Faça a vistoria da sua bike agora</h1>
+                    <h3>Simples e rápido</h3>
+                </article>
+                <Link to={'/vistoria'}>Começar</Link>
             </main>
         </>
     )
