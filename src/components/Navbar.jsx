423d0c1380b08b03594894bb0261b7e769f50e68
Cauã Alencar Rojas Romero <cauarojas@hotmail.com>
2023-09-09 16:27:22 -0300

    added some basic styling to the page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,23 @@
+import { Link } from 'react-router-dom'
+import portoLogo from '../assets/porto.svg'
+import '../styles/Navbar.css'
 export default function Navbar() {
     return (
         <>
-            <nav>NavBar</nav>
+            <nav>
+                <Link to="/">
+                    <img src={portoLogo} alt="Logo da Porto" />
+                </Link>
+                <div className="links">
+                    <Link to="/como-funciona">Como Funciona</Link>
+                    <Link to="/planos">Planos</Link>
+                    <Link to="/faq">Duvidas Frequentes</Link>
+                    <Link to="/nexio">Equipe</Link>
+                    <Link className="login" to="/login">
+                        Entrar
+                    </Link>
+                </div>
+            </nav>
         </>
     )
 }
