a735f0f2552ba24ae66def4aa5e025b83f2018ee
Cauã Alencar Rojas Romero <cauarojas@hotmail.com>
2023-09-10 17:44:22 -0300

    updated paths in router

--- src/routes/BikeRegistration.jsx
+++ src/routes/BikeRegistration.jsx
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom'
+
 export default function BikeRegistration() {
     return (
         <>
             <h1>Bike Registration</h1>
+            <Link to={'/vistoria'}>Ir para vistoria</Link>
         </>
     )
 }
