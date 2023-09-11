fd40c9a8c209013b2e25f6c7830c1c32439bbf50
CauaRojas <cauarojas@hotmail.com>
2023-09-11 09:17:49 -0300

    fixed misspelled file

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -1,19 +1,23 @@
-import PropTypes from "prop-types";
-import '../styles/infoCard.css';
+import PropTypes from 'prop-types'
+import '../styles/InfoCard.css'
 
 export default function InfoCard(props) {
-	return (
-		<div className="infocard-box">
-			<img className="infocard-img" src={props.sourceImage} alt={props.altImage} />
-			<p className="infocard-title">{props.title}</p>
-			<p className="infocard-text">{props.text}</p>
-		</div>
-	);
+    return (
+        <div className="infocard-box">
+            <img
+                className="infocard-img"
+                src={props.sourceImage}
+                alt={props.altImage}
+            />
+            <p className="infocard-title">{props.title}</p>
+            <p className="infocard-text">{props.text}</p>
+        </div>
+    )
 }
 
 InfoCard.propTypes = {
-	sourceImage: PropTypes.string.isRequired,
-	altImage: PropTypes.string.isRequired,
-	title: PropTypes.string.isRequired,
-	text: PropTypes.string.isRequired,
-};
+    sourceImage: PropTypes.string.isRequired,
+    altImage: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+}
