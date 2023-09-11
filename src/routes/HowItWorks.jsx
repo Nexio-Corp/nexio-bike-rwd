62415c575845c6f7f8a7fb2f1e55b7f74db5c2bc
adrian-siv <adrian.ssiv@gmail.com>
2023-09-10 23:05:36 -0300

    Att image path

--- src/routes/HowItWorks.jsx
+++ src/routes/HowItWorks.jsx
@@ -9,19 +9,19 @@ export default function HowItWorks() {
 				mão
 			</h2>
 			<InfoCard
-				sourceImage="public\static\little-bike.svg"
+				sourceImage="/static/little-bike.svg"
 				altImage="Desenho de uma bike"
 				title="Escolha seu plano"
 				text="Oferecemos o plano perfeito para se adequar ao seu perfil de ciclista. Faça a sua escolha e aproveite suas pedaladas com total tranquilidade e segurança."
 			/>
 			<InfoCard
-				sourceImage="public\static\form.svg"
+				sourceImage="/static/form.svg"
 				altImage="Desenho de um formulário sendo preenchido"
 				title="Preencha os seus dados"
 				text="Para inclusão na sua apólice, solicitaremos apenas algumas informações essenciais. Não se preocupe, o processo é rápido e simples!"
 			/>
 			<InfoCard
-				sourceImage="public\static\clipboard.svg"
+				sourceImage="/static/clipboard.svg"
 				altImage="Prancheta com checklist"
 				title="Faça a vistoria on-line"
 				text="Após fornecer suas informações, você só precisará enviar algumas fotos de diferentes ângulos da sua bike, e em questão de minutos, o seu seguro estará pronto."
