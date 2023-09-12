03fae81ebb91df1b535a2b09ab58b1bd46e0bb64
jaciteixeira <rm99627@fiap.com.br>
2023-09-12 12:06:48 -0300

    add links as logos

--- src/components/TeammateCard.jsx
+++ src/components/TeammateCard.jsx
@@ -8,14 +8,20 @@ export default function TeammateCard(props) {
       <p>{props.nome}</p>
       <p>{props.rm}</p>
       <div className="logo-box">
-        <img src="/static/github-logo.svg" alt="logo github" />
-        <img src="/static/linkedin-logo.svg" alt="logo linkedin" />
+        <a href={props.github} target="_blank"  rel="noreferrer" >
+          <img src="/static/github-logo.svg" alt="logo github" />
+        </a>
+        <a href={props.linkedin} target="_blank"  rel="noreferrer">
+          <img src="/static/linkedin-logo.svg" alt="logo linkedin" />
+        </a>
       </div>
     </div>
   );
 }
 
 TeammateCard.propTypes = {
+  github: PropTypes.string.isRequired,
+  linkedin: PropTypes.string.isRequired,
   sourceImage: PropTypes.string.isRequired,
   altImage: PropTypes.string.isRequired,
   nome: PropTypes.string.isRequired,
