2812a0cf277bf1892de2f6b6716e3617a4bcbedf
PedroPaterno <rm99726@fiap.com.br>
2023-09-12 19:25:36 -0300

    Terminada a página bike-registration

--- src/routes/BikeRegistration.jsx
+++ src/routes/BikeRegistration.jsx
@@ -1,10 +1,46 @@
 import { Link } from 'react-router-dom'
+import '../styles/BikeRegistration.css'
 
 export default function BikeRegistration() {
     return (
-        <>
-            <h1>Bike Registration</h1>
-            <Link to={'/vistoria'}>Ir para vistoria</Link>
+        <>  <div className="h1-registration">
+                <h1>Cadastro Bike</h1>
+                <h2>Preecha todos os campos a baixo para cadastrar sua bicicleta</h2>    
+            </div>
+            <main className="bike-registration">
+                <form>
+                    <div className="form-group">    
+                        <label htmlFor="Marca">Marca*</label>
+                        <select name="Marca" id="Marca" required>
+                            <option value="Trek">Trek</option>
+                            <option value="Specialized">Specialized</option>
+                            <option value="Giant">Giant</option>
+                            <option value="Cannondale">Cannondale</option>
+                            <option value="Scott">Scott</option>
+                        </select> 
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="Modelo">Modelo*</label>
+                        <input type="text" required/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="Modificacoess">Modificações*</label>
+                        <input type="text" required/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="Valor">Valor*</label>
+                        <input type="text" required/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="Modelo">Nº Chassi*</label>
+                        <input type="text" required/>
+                    </div>
+                </form>
+                <div className="botao-finalizar">
+                    <Link to={'/vistoria'}>Finalizar</Link>
+                </div>
+            </main>
+            
         </>
     )
 }
