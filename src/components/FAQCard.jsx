9b7148c31b0f0e02b7a9aaa53e5b9e21bd2905d7
SabrinaFaustino <rm99570@fiap.com.br>
2023-09-12 15:54:58 -0300

    FAQ page styled

--- src/components/FAQCard.jsx
+++ src/components/FAQCard.jsx
@@ -3,13 +3,16 @@ import '../styles/FAQCard.css'
 
 export default function FAQCard(props) {
     return (
-        <div className="faqcard-box">
+        <div className="faqcard-container">
             <div className="faqcard-title-box">
                 <p className="faqcard-title">{props.title}</p>
+                <p className="faqplus">+</p>
             </div>
             <div className="faqcard-text-box">
                 <p className="faqcard-text">{props.text}</p>
-                {props.list}
+                <div className="faqcard-list">
+                    {props.list}
+                </div>
                 <p className="faqcard-subtext">{props.subtext}</p>
             </div>
         </div>
@@ -19,6 +22,6 @@ export default function FAQCard(props) {
 FAQCard.propTypes = {
     title: PropTypes.string.isRequired,
     text: PropTypes.string.isRequired,
-    list: PropTypes.string.isRequired,
+    list: PropTypes.array.isRequired,
     subtext: PropTypes.string.isRequired
 }
