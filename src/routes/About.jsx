6bc0e07c309a48e7aa2abe11620a6a6a46a501dc
jaciteixeira <rm99627@fiap.com.br>
2023-09-11 23:40:02 -0300

    page equipe/sobre

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,7 +1,44 @@
+import perfilAdrian from '/static/adrian.png'
+import perfilCaua from '/static/adrian.png'
+
+import '../styles/About.css'
 export default function About() {
     return (
         <>
-            <h1>About</h1>
+            <main className='equipe'>
+                <div className='titulo'>
+                    <h1>Conheça nossa equipe</h1>
+                    <p>Uma equipe altamente eficiente, dedicada <br/>exclusivamente a buscar o melhor para cada cliente.</p>
+                </div>
+                <div className="integrantes">
+                    
+                    <div className="adrian">
+                        <img src={perfilAdrian} alt="Integrante Adrian" />
+                        <p>Adrian Satiro</p>
+                        <p>RM97784</p>
+                    </div>
+                    <div className='caua'>
+                        <img src={perfilCaua} alt="Integrante Cauã" />
+                        <p>Cauã Alencar</p>
+                        <p>RM98638</p>
+                    </div>
+                    <div className='jaci'>
+                        <img src={perfilCaua} alt="Integrante Cauã" />
+                        <p>Jaci Teixeira</p>
+                        <p>RM99627</p>
+                    </div>
+                    <div className='sabrina'>
+                        <img src={perfilCaua} alt="Integrante Cauã" />
+                        <p>Sabrina Faustino</p>
+                        <p>RM99570</p>
+                    </div>
+                    <div className='pedro'>
+                        <img src={perfilCaua} alt="Integrante Cauã" />
+                        <p>Pedro Henrique</p>
+                        <p>RM99726</p>
+                    </div>
+                </div>
+            </main>
         </>
     )
 }
